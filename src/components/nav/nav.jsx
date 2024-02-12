30ddf5d30600f5eb8b7f090c622e0a3e5ac18b2a
Elshater6498 <elshaterm247@gmail.com>
2024-02-12 19:26:15 +0200

    navbar

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,59 +1,67 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import '../../App.css'
-import './nav.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../../App.css";
+import "./nav.css";
 
 function Nav() {
-  const [isMenuVisible, setIsMenuVisible] = useState(false)
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
   return (
-    <header className='header' id='header'>
-      <nav className='nav container'>
+    <header
+      className="w-full bg-transparent fixed top-0 left-0 z-50"
+      id="header"
+    >
+      <nav className="h-14 flex justify-between items-center max-w-4xl mx-auto px-4">
         <Link
-          to='/'
-          className='nav__logo'
-          alt='...'
+          to="/"
+          className="text-text-color-light z-50 uppercase font-bold tracking-wide flex items-center gap-1 transition duration-500 hover:text-body-color"
+          alt="logo"
           onClick={() => setIsMenuVisible(false)}
         >
-          <i className='bx bx-planet'></i> YO
+          <i className="bx bx-planet font-semibold"></i> elshater
         </Link>
 
-        <div className='nav__btns'>
+        <div className="flex items-center">
+          {/* links */}
           <div
-            className={isMenuVisible ? 'nav__menu show-menu' : 'nav__menu'}
-            id='nav-menu'
+            className={
+              isMenuVisible
+                ? "fixed top-0 -right-[100%] bg-body-color flex justify-center py-20 transition duration-500 w-full h-full"
+                : "nav-menu"
+            }
+            id="nav-menu"
           >
-            <ul className='nav__list'>
-              <li className='nav__item'>
+            <ul className="nav__list">
+              <li className="nav__item">
                 <Link
-                  to='/'
-                  className='nav__link'
+                  to="/"
+                  className="nav__link"
                   onClick={() => setIsMenuVisible(false)}
                 >
                   Home
                 </Link>
               </li>
-              <li className='nav__item'>
+              <li className="nav__item">
                 <Link
-                  to='/about'
-                  className='nav__link'
+                  to="/about"
+                  className="nav__link"
                   onClick={() => setIsMenuVisible(false)}
                 >
                   About
                 </Link>
               </li>
-              <li className='nav__item'>
+              <li className="nav__item">
                 <Link
-                  to='/projects'
-                  className='nav__link'
+                  to="/projects"
+                  className="nav__link"
                   onClick={() => setIsMenuVisible(false)}
                 >
                   Projects
                 </Link>
               </li>
-              <li className='nav__item'>
+              <li className="nav__item">
                 <Link
-                  to='/contact'
-                  className='nav__link'
+                  to="/contact"
+                  className="nav__link"
                   onClick={() => setIsMenuVisible(false)}
                 >
                   Contact
@@ -62,16 +70,16 @@ function Nav() {
             </ul>
           </div>
           <div
-            className='nav__toggle'
-            id='nav-toggle'
+            className="nav__toggle"
+            id="nav-toggle"
             onClick={() => setIsMenuVisible(!isMenuVisible)}
           >
-            <i className='bx bx-grid-alt'></i>
+            <i className="bx bx-grid-alt"></i>
           </div>
         </div>
       </nav>
     </header>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "first-color": "hsl(184, 55%, 55%)",
+        "button-color": "hsl(0, 0%, 17%)",
+        "button-color-alt": "hsl(0, 0%, 21%)",
+        "title-color": "hsl(0, 0%, 15%)",
+        "text-color": "hsl(0, 0%, 35%)",
+        "text-color-light": " hsl(0, 0%, 55%)",
+        "body-color": "hsl(0, 0%, 99%)",
+        "container-color": " #fff",
+        "border-color": "hsl(0, 0%, 94%)",
+      },
+    },
   },
   plugins: [],
 };
