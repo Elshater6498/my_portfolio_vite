86a513e834bdbc2ea155e73f00e3df8fb7a51cf4
Elshater6498 <elshaterm247@gmail.com>
2024-03-04 17:40:39 +0200

    text color in footer component

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div className='w-full text-center'>
-      <p className='text-xs'>&copy;2022 - elshater</p>
+    <div className="w-full text-center text-body-color">
+      <p className="text-xs">&copy;2022 - elshater</p>
     </div>
-  )
+  );
 }
-
-export default Footer
+export default Footer;
