3f9323ca7a66449a837b8668a45cf6290b49a02e
Elshater6498 <elshaterm247@gmail.com>
2024-06-12 14:23:38 +0300

    some edits on projects

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 function Projects() {
   const projects = [
     {
@@ -67,7 +66,6 @@ function Projects() {
       ],
     },
   ];
-
   return (
     <section className="min-h-[calc(100vh-21.7px)] flex items-center justify-center gap-10 flex-wrap px-2 py-20 text-body-color">
       {projects.map((item) => (
@@ -77,7 +75,9 @@ function Projects() {
         >
           <div className="flex flex-col justify-between items-center gap-2 h-full">
             <div className="flex flex-col items-center text-center">
-              <h4 className="mb-2 text-2xl border-b whitespace-nowrap pb-2 px-4">{item.title}</h4>
+              <h4 className="mb-2 text-2xl border-b whitespace-nowrap pb-2 px-4">
+                {item.title}
+              </h4>
               <p className="text-sm">{item.details}</p>
             </div>
             <div className="flex border-t pt-2 w-full justify-center flex-wrap">
